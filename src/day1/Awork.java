1dbfdd500e191944ef17d95e7e9bea89c73021dd
ViktoriiaMoskul <moskul1v@gmail.com>
2021-07-26 15:35:24 -0400

    change 2

--- src/day1/Awork.java
+++ src/day1/Awork.java
@@ -3,5 +3,6 @@ package day1;
 public class Awork {
     //change1 was done
     //change2 was done
+    //change3 was done
 
 }
---
+++ src/day1/FeatureBranchFile.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class FeatureBranchFile {
+    //change 1 was done
+
+
+
+}
